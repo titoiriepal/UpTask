b7080d5556dce8d5e3668b9d72e1aa23fea1033b
DESKTOP-LO3FN3B\TitoI <titoiriepal@gmail.com>
2022-04-17 18:42:20 +0200

    change task status

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -67,6 +67,32 @@ class TareaController{
     public static function actualizar(){
 
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            //Validar que el proyecto exista
+            $proyecto = Proyecto::where('url', $_POST['url']);
+            session_start();
+            if(!$proyecto || $proyecto->propietarioid !== $_SESSION['id']){
+                $respuesta = [
+                    'tipo' => 'error',
+                    'mensaje' => 'Hubo un error al actualizar la tarea'
+                ];
+
+                echo json_encode($respuesta);
+                return;
+                
+            }
+            $tarea = new Tarea($_POST);
+
+            $resultado = $tarea->guardar();
+            if($resultado){
+                $respuesta = [
+                    'tipo' => 'exito',
+                    'id' => $tarea->id,
+                    'proyectoid' => $proyecto->id,
+                    'mensaje' => 'Actualizado correctamente'
+                ];
+                echo json_encode(['respuesta' => $respuesta]);
+            }
+
             
         }
 
